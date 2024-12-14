8c6933e8d3e093f7e2a3c8a64bf94f4539be4eca
NathanKassahun1234 <Kassahunny@uchicago.edu>
2024-12-14 03:27:18 +0000

    37_p

--- db/migrate/20241124012352_create_users.rb
+++ db/migrate/20241124012352_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :username
+      # t.string :username
       t.string :email
       t.string :encrypted_password
       t.boolean :private
